9fd5de145f4062b0aed4ce0a3ad94b62b907ecfd
Erik Carlson <carlsonerikw@gmail.com>
2020-03-05 20:30:10 -0800

    Add initial set of immediately needed dependencies. Add event listener for local storage.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-/* ------------- CSS ------------- */
+/* ------------- Reset Default CSS ------------- */
 import './reset.css';
 
 function Coddit() {
+  const settingsChanged = (event) => {
+    console.log('local storage settings changed', event);
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Add event to listen for local storage changes
+      window.addEventListener('storage', settingsChanged)
+      return function cleanup() {
+         // Clean-up event listeners
+        window.removeEventListener('storage', settingsChanged);
+      };
+    }
+  });
+
   return (
     <div>coddit</div>
   )
