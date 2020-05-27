b03d193bc1b6e522b59d61edd6e161508d5f074e
Erik Carlson <carlsonerikw@gmail.com>
2020-05-27 19:36:06 -0400

    Pull colors out into theme file. Set-up differentiation between general and language specific styled components

---
+++ src/styled-components/keywords/index.js
@@ -0,0 +1,17 @@
+import {
+  KarmaScore,
+  CommentListItem,
+  String,
+  StringLink,
+  StringListItem,
+  Submitter,
+} from './general';
+
+export {
+  KarmaScore,
+  CommentListItem,
+  String,
+  StringLink,
+  StringListItem,
+  Submitter,
+};
---
+++ src/styled-components/theme.js
@@ -0,0 +1,14 @@
+
+const STRING = 'green';
+const SUBMITTER = 'red';
+const COMMENT = 'grey';
+const POSITIVE_VOTES = 'orange';
+const NEGATIVE_VOTES = 'purple';
+
+export {
+  COMMENT,
+  STRING,
+  SUBMITTER,
+  POSITIVE_VOTES,
+  NEGATIVE_VOTES
+};
